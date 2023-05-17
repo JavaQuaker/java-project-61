4f054c5e87589ba9e74e954a2a794ff76147671c
Alexey <leha_s78@mail.ru>
2023-05-17 21:56:55 +0300

    add final variable UP_LIMIT (Calc)

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -5,13 +5,14 @@ import hexlet.code.Engine;
 public class Calc {
     public static final int STR_ARR = 3;
     public static final int COL_ARR = 2;
+    public static final int UP_LIMIT = 100;
     public static void calculation() {
         String[][] arr = new String[STR_ARR][COL_ARR];
         System.out.println("What is the result of the expression?");
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < STR_ARR; i++) {
 
-            int ranNmb1 = (int) (Math.random() * 100);
-            int ranNumb2 = (int) (Math.random() * 100);
+            int ranNmb1 = (int) (Math.random() * UP_LIMIT);
+            int ranNumb2 = (int) (Math.random() * UP_LIMIT);
             int[] array = {ranNmb1 + ranNumb2, ranNmb1 - ranNumb2, ranNmb1 * ranNumb2};
             int result = (int) (Math.random() * array.length);
             String[] operations = {"+", "-", "*"};
