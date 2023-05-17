a213ac43e1259b0a5322d09e6fbb62b4a8e7f97b
Alexey <leha_s78@mail.ru>
2023-05-17 15:44:32 +0300

    update

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Engine {
     public static String answer;
-
     public static void logic(String[][] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.println("Question: " + arr[i][0]);
