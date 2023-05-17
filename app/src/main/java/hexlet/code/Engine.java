0b9d7ec0f13a5dd6473383cca85072d7d95a48d8
Alexey <leha_s78@mail.ru>
2023-05-17 15:29:12 +0300

    update (add "your answer: ")

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -9,14 +9,14 @@ public class Engine {
     public static void logic(String[][] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.println("Question: " + " " + arr[i][0]);
-
+            System.out.print("Your answer: ");
             Scanner scanner = new Scanner(System.in);
             if (scanner.hasNextLine()) {
                 Engine.answer = scanner.nextLine();
                 if (arr[i][1].equals(answer)) {
                     String result = (answer.equals(arr[i][1])) ? "Correct!" : answer + " " + "is wrong answer ;(. "
                             + "Correct answer was" + arr[i][1] + ".\n" + "Let's try again, " + " " + Cli.name + "!";
-                    System.out.println("Your answer: " + result);
+                    System.out.println(result);
 
                 } else {
                     if (!answer.equals(arr[i][1])) {
