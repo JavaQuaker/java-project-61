f0647a476ea6afe4faf6f8f46a640789d367f4b9
Alexey <leha_s78@mail.ru>
2023-05-23 18:54:04 +0300

    update (add "Welcome ..." Cli)

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,6 +2,7 @@ package hexlet.code;
 import java.util.Scanner;
 public class Cli {
     public static void greeting() {
+        System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         Scanner scanner = new Scanner(System.in);
         if (scanner.hasNextLine()) {
