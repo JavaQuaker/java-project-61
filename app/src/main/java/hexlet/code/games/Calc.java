6a81dd9fcafaa8f57932df8bd397bc13a47d5e72
Alexey <leha_s78@mail.ru>
2023-05-23 20:04:07 +0300

    update (changing the scope of a variable String[] operation)

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -4,8 +4,13 @@ import hexlet.code.Engine;
 import hexlet.code.Utils.RandomClass;
 
 public class Calc {
-    static String[] operations = {"+", "-", "*"};
+    private static String[] operations = {"+", "-", "*"};
     public static final int UP_LIMIT = 100;
+
+    public static String[] getOperations() {
+        return operations;
+    }
+
     public static void defineRanNumb() {
         String[][] questionAnswer = new String[Engine.ROUNDS_COUNT][Engine.COL_ARR];
 
@@ -14,7 +19,7 @@ public class Calc {
             int ranNumb2 = RandomClass.searchRanNumber(UP_LIMIT, 0);
             int indexOperation = RandomClass.searchRanNumber(0, 2);
 
-            questionAnswer[i] = new String[]{Integer.toString(ranNmb1) + " " + operations[indexOperation]
+            questionAnswer[i] = new String[]{Integer.toString(ranNmb1) + " " + getOperations()[indexOperation]
                     + " " + Integer.toString(ranNumb2),
                     Integer.toString(calculate(ranNmb1, indexOperation, ranNumb2))};
         }
@@ -23,7 +28,7 @@ public class Calc {
     }
     public static int calculate(int ranNmb1, int indexOperation, int ranNumb2) {
         int result = 0;
-        switch (operations[indexOperation]) {
+        switch (getOperations()[indexOperation]) {
             case "+":
                 result = ranNmb1 + ranNumb2;
                 break;
